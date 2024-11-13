3c3d07bc07e133d1579825dfbbd087a4c43a80ec
Alperen <bitirgenalperen@gmail.com>
2024-11-13 23:57:35 +0300

    env variables

--- backend/envVariables.js
+++
@@ -1,5 +0,0 @@
-const uri = "mongodb+srv://bitirgenalperen:SK6SD6srTVoPMDBd@backenddb.q2mcn.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB";
-const secret = "realestate";
-
-
-export {uri, secret};
--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,15 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcrypt');
 const cors = require('cors');
-const { uri, secret } = require('./envVariables.js');
 const mongoose = require('mongoose');
 const ObjectId = mongoose.Types.ObjectId;
 const { checkRole } = require('./utils/checkRole.js');
 const User = require('./models/User');
 const Project = require('./models/Project');
 const Booking = require('./models/Booking');
+
+const dotenv = require('dotenv');
+dotenv.config();
 
 // Initialize the app
 const app = express();
@@ -25,7 +27,7 @@ app.use(cors({
 
 // Session configuration
 app.use(session({
-  secret: secret,
+  secret: process.env.SECRET,
   resave: false,
   saveUninitialized: false,
   cookie: {
@@ -38,7 +40,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // MongoDB Connection
-mongoose.connect(uri)
+mongoose.connect(process.env.URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 
